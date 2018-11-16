ea1185088c908fccec222217789df9af3e386994
Gábor Horváth <ghorvath@geomant.com>
2018-11-16 09:12:11 +0100

    Update ServiceFabricCommands.ps1

--- ServiceFabricCommands.ps1
+++ ServiceFabricCommands.ps1
@@ -12,15 +12,16 @@ Connect-ServiceFabricCluster -ConnectionEndpoint "servicefabric-geolabs.westeuro
 Connect to buzzeasy-prod SF:
 Connect-ServiceFabricCluster -ConnectionEndpoint "buzzeasy-prod.westeurope.cloudapp.azure.com:19000" -AzureActiveDirectory -ServerCertThumbprint "A8F0186668486C136DECF6FE4B80B8435D1B8754"
 
+Connect to buzzplus-prod SF:
+Connect-ServiceFabricCluster -ConnectionEndpoint "buzzplus-prod.westeurope.cloudapp.azure.com:19000" -AzureActiveDirectory -ServerCertThumbprint "424080AB9D61B5F145C5F97EAA8BB2E24753161A"
+
 ### Templates ###
 
 Template for Deploying new tenant:
 New-ServiceFabricApplication -ApplicationName fabric:/CloudContactExpert_ghorvath -ApplicationTypeName "CloudContactExpertType" -ApplicationTypeVersion "1.0.0.4" 
 -ApplicationParameter @{ServiceBusConnectionString='[Connection string specific for Generali]'; ApplicationInsightsKey='[Common Application Insights key to log CE messages with]'}
 
-New-ServiceFabricApplication -ApplicationName fabric:/BuzzPlus_Test -ApplicationTypeName "BuzzPlusType" -ApplicationTypeVersion "1.0.0.20180910.5" 
--ApplicationParameter
-
+New-ServiceFabricApplication -ApplicationName fabric:/BuzzPlus_Test -ApplicationTypeName "BuzzPlusType" -ApplicationTypeVersion "1.0.0.20180910.5" -ApplicationParameter
 
 New-ServiceFabricApplication -ApplicationName fabric:/BuzzPlus_Test2 -ApplicationTypeName "BuzzPlusType" -ApplicationTypeVersion "1.0.0" -ApplicationParameter @{ServiceBusConnectionString='Endpoint=sb://buzzplus-dev-ns-test2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=7J8IcSBAepTHN4L9JAwzzUd4RR+Nrj2Ke4t7f1slPI0='; ApplicationInsightsKey='123657a0-f337-4232-9bd6-2da2ac2a9ec7'; PublicAddressRoot='https://cecloud-test.geomant.cloud'; AgentFabricEndpointUrl='https://cecloud-test.geomant.cloud/BuzzPlus_Test2/BuzzPlusEngine/api/fabric/GetState'; AuthorizationServiceAuthority='https://geoauth-dev.azurewebsites.net'; AuthorizationServiceClientId='client.bp.cecloud-test.Tenant2'; AuthorizationServiceClientSecret='204df46d9ef94860b1c49b9193936d58'; AuthorizationServiceClientScope='bp.access'; AuthorizationServiceAudience='bp.access'; AuthorizationTokenEndpoint='https://geoauth-dev.azurewebsites.net/connect/token'; ChatProxy_PartitionCount='1'; ChatProxy_MinReplicaSetSize='3'; ChatProxy_TargetReplicaSetSize='3'; ServiceBusConnectionString_BuzzeasyIn='Endpoint=sb://buzzeasychatcld1sb.servicebus.windows.net/;SharedAccessKeyName=cloudconnector_3;SharedAccessKey=YqDwPeN7J0NU67QUG5pG3c0UftpxIJ8eNcfI82Xjm+A=;EntityPath=cloudconnector_3_1'; ServiceBusConnectionString_BuzzeasyOut='Endpoint=sb://buzzeasychatcld1sb.servicebus.windows.net/;SharedAccessKeyName=cloudconnector_3;SharedAccessKey=6Ns78OMR8IIaP8YcbHQXnN9mG24mkZ3Z9e0DCB8dU/g=;EntityPath=accqueuechat'; DocumentStoreService_InstanceCount='-1'; BlobStorageAccountName='docsdevtest2'; BlobStorageAccountKey='hAcZu+bWlyw8gQ/lPT09VqGRnbF7iSELnwBdI/BTA487xjWi/s2Twth0W6Zpheur4LGi+4TETjTSeaT5BUeLvw=='; BlobStorageContainerName='documentstoreservice-container-test2'; KeyVaultClientId='5b3d648a-6494-4f2a-8fa5-2282e512e5f6'; KeyVaultClientSecret='9DuwWg2azbqrMhkeoARx/wCGCYwTZQZ5W6ZT8rCqpCc='; KeyVaultRSAKeyIdentifier='https://sf-geolabs-vault.vault.azure.net/keys/storage-encryption-key/4ac8c9ec96c444ac83d10744c765c601'; AgentFrontEnd_InstanceCount='1'; NodeJsEnvironment='development'; BuzzPlusEngine_PartitionCount='1'; BuzzPlusEngine_MinReplicaSetSize='3'; BuzzPlusEngine_TargetReplicaSetSize='3'; ResourceHeartbeat_TimeoutSeconds='60'; ChatFrontEndUrl='https://cecloud-test.geomant.cloud/BuzzPlus_ChatConversationWindow_Test2/ChatFrontEnd/'; CQFrontEndUrl=''; ResourceMaxConcurrentWorkItems='3'; ExternalWorkAssignmentEngineUrl=''; AuthenticationEnabled='true'; }
 
@@ -48,6 +49,21 @@ Remove-ServiceFabricApplicationPackage -ApplicationPackagePathInImageStore "Clou
 Start-ServiceFabricApplicationUpgrade -ApplicationName fabric:/CloudContactExpert -ApplicationTypeVersion 1.0.2 -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000   -FailureAction Rollback -Monitored
 
 Get-ServiceFabricApplicationUpgrade fabric:/CloudContactExpert
+
+# App params generation #
+
+
+$params_pa = .\Cre
+ateAppParams.ps1 -environment Prod -customerName pademo -EnvironmentSubscriptionId 96cd3ac2-1b22-4639-9eaa-94f17c7854ca
+-ServicePrincipalTenantId ddb0d7ac-52f1-403c-a926-9bca83caa4c1 -ServicePrincipalClientId 9f713010-7c41-4660-8097-1d0167f
+c6d1e -ServicePrincipalPassword bqWhLZqqqDdDSOfJEBfa7SJc3Cw6n9QBxJYwAGEaJkY= -DomainAddress cecloud-test.geomant.cloud -
+BuzzeasyResourceGroupName buzzeasychat-tenant-cld1-prd-rg -BuzzeasyServiceBusNamespace buzzeasychatcld1sb -BuzzeasyOutbo
+undQueueName cloudconnector_25_1 -BuzzeasyOutboundQueueSAS cloudconnector_25 -BuzzeasyInboundQueueName accqueuechat -Buz
+zeasyInboundQueueSAS cloudconnector_25 -NotEncryptParams
+
+
+(ConvertFrom-Json
+$params_pa).psobject.properties | Foreach { $hashtable_pa[$_.Name] = $_.Value }
 
 ### Application parameters ###
 
