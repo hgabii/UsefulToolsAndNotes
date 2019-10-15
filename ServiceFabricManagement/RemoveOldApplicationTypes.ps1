30d0ccbd0d138d9dfd4d59c5f8803514f7ba2e21
Gabor Horvath <ghorvath@geomant.com>
2019-10-15 10:50:18 +0200

    Add SF App Type remove script

---
+++ ServiceFabricManagement/RemoveOldApplicationTypes.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory = $true)][string]$AppTypeName,
+    [Parameter(Mandatory = $false)][int]$CountToKeep
+)
+
+if(!$CountToKeep)
+{
+    $CountToKeep = 2
+}
+
+#resolve all app types
+$appTypes = Get-ServiceFabricApplicationType -ApplicationTypeName $AppTypeName
+$deployedAppArray = @()
+foreach($appType in $appTypes){    
+    #try to find the match with any of installed applications
+    $match = Get-ServiceFabricApplication -ApplicationTypeName $appType.ApplicationTypeName | Where-Object {$_.ApplicationTypeVersion -eq $appType.ApplicationTypeVersion}
+    if(!$match)
+    {
+        $oldApp = new-object psobject -property @{
+            ApplicationTypeName = $appType.ApplicationTypeName
+            ApplicationTypeVersion  = $appType.ApplicationTypeVersion 
+        }
+        $deployedAppArray += $oldApp
+    }    
+}
+
+$uniqueAppTypes = $deployedAppArray | Group-Object "ApplicationTypeName" | Where-Object { $_.Count -gt $CountToKeep } | Select-Object -ExpandProperty Name
+
+foreach($appType in $uniqueAppTypes){ 
+    $versionsToRemove = $deployedAppArray | Where-Object {$_.ApplicationTypeName -eq $appType}
+    $toRemoveCount =  $versionsToRemove.Length - $CountToKeep
+    $versionsToRemove = $versionsToRemove | Select-Object -First $toRemoveCount
+
+    foreach($appToRemove in $versionsToRemove){
+        Write-Host "Removing $($appToRemove.ApplicationTypeName) $($appToRemove.ApplicationTypeVersion)"
+        Unregister-ServiceFabricApplicationType -ApplicationTypeName $appToRemove.ApplicationTypeName -ApplicationTypeVersion $appToRemove.ApplicationTypeVersion -Force
+    }
+}
---
+++ ServiceFabricManagement/RemoveUnusedApplicationTypes.ps1
@@ -0,0 +1,16 @@
+param(
+    [Parameter(Mandatory = $true)][string]$AppTypeName
+)
+
+#resolve all app types
+$appTypes = Get-ServiceFabricApplicationType -ApplicationTypeName $AppTypeName
+foreach($appType in $appTypes)
+{
+   #try to find the match with any of installed applications
+   $match = Get-ServiceFabricApplication -ApplicationTypeName $appType.ApplicationTypeName | Where-Object {$_.ApplicationTypeVersion -eq $appType.ApplicationTypeVersion}
+   if(!$match)
+   {
+       Write-Host "Deleting $($appType.ApplicationTypeName) $($appType.ApplicationTypeVersion)"
+       Unregister-ServiceFabricApplicationType -ApplicationTypeName $appType.ApplicationTypeName -ApplicationTypeVersion $appType.ApplicationTypeVersion -Force -Confirm
+   }    
+}
